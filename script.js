275bd9fd319e11634346587365eaffb05666d645
rizwangul-hub <rizwangul-hub>
2026-01-28 17:33:57 +0500

    file

---
+++ script.js
@@ -0,0 +1,35 @@
+//Assignment : 1 JavaScript
+//Submitted By : Rizwan Ullah
+
+//alert()
+alert("Hello Word")
+
+//console
+console.log("Hello Word");
+
+//var :  //can be redeclared and reasign
+var studentName="Farhan"
+console.log(studentName);  //show in console
+document.write(studentName) //show on webpage
+
+//.........................................................................................
+
+//let :  //can be reasign but not redelared
+let courseName="Webdevelopment"
+console.log(courseName);  //show in console
+document.write(courseName) //show on webpage
+courseName="Graphic Disgin"//updated 
+console.log(courseName);
+
+//........................................................................................
+
+//const :  // not redelared and not reasign
+const batchNumber= "Morning Batch"
+console.log(batchNumber);    //show in console
+document.write(batchNumber) //show on webpage
+batchNumber="Evening Batch" //not updated
+console.log(batchNumber);// show erro
+
+//.......................................................................................
+// END OF ASSIGNMENT 1
+
